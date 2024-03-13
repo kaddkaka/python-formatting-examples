e6dc6bd9e0b72a15c7fd8895c65859fc2deebfee
David Moberg <kaddkaka@gmail.com>
2024-03-13 12:35:53 +0100

    update script

--- dupl_file.sh
+++ dupl_file.sh
@@ -1,29 +1,45 @@
 rm -r black
 rm -r yapf
 rm -r yapf-google
+rm -r yapf-facebook
+rm -r yapf-yapf
 cp -rT unformatted black
 cp -rT unformatted yapf
 cp -rT unformatted yapf-google
+cp -rT unformatted yapf-facebook
+cp -rT unformatted yapf-yapf
 
-time black black
-echo "~~~~~~~~~~~~~~~~~~ Black ~~~~~~~~~~~~~~~~~~"
+time black --line-length=80 black
+echo "^^^^^^^^^^^^^^^^^^ Black ^^^^^^^^^^^^^^^^^^"
+
 time yapf -i --recursive yapf
-echo "~~~~~~~~~~~~~~~~~~ yapf ~~~~~~~~~~~~~~~~~~"
+echo "^^^^^^^^^^^^^^^^^^ yapf ^^^^^^^^^^^^^^^^^^"
+
 time yapf -i --recursive --style=google yapf-google
-echo "~~~~~~~~~~~~~~~~~~ yapf-google ~~~~~~~~~~~~~~~~~~"
+echo "^^^^^^^^^^^^^^^^^^ yapf-google ^^^^^^^^^^^^^^^^^^"
+
+time yapf -i --recursive --style=facebook yapf-facebook
+echo "^^^^^^^^^^^^^^^^^^ yapf-facebook ^^^^^^^^^^^^^^^^^^"
+
+time yapf -i --recursive --style=yapf yapf-yapf
+echo "^^^^^^^^^^^^^^^^^^ yapf-yapf ^^^^^^^^^^^^^^^^^^"
 
 # globstar is not sorted per directory
 #nvim **/*.py
 fd .py | xargs nvim -S <(cat << EOF
 :e unformatted/0.py
-:below sb black/0.py
-:vertical below sb yapf/0.py
+:belowright sb black/0.py
+:belowright split yapf/0.py
+:belowright split yapf-google/0.py
+:belowright vsplit yapf-facebook/0.py
+:norm! k
+:belowright vsplit yapf-yapf/0.py
 :windo set scrollbind
 :nnoremap <right> <cmd>windo bn<cr>
 :nnoremap <left> <cmd>windo bp<cr>
 :autocmd! lspconfig
 :LspStop
-:set laststatus=3
+":set laststatus=3
 EOF
 )
 
