3b56d4cba45f498f200b53b834efc47942a097ac
David Moberg <kaddkaka@gmail.com>
2024-03-18 00:05:03 +0100

    unformatted: add two examples

---
+++ unformatted/class-object.py
@@ -0,0 +1,5 @@
+# custom_object.py
+
+class Foo(object):
+    def bar(self, value: str) -> str:
+        return "value is {}".format(value)
---
+++ unformatted/own_line_comments.py
@@ -0,0 +1,4 @@
+(
+    # A comment in the middle
+    some_example_var and some_example_var not in some_example_var
+)
